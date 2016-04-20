850f8fcdda6ca06522959c81322346de084ccec3
naki <inakilanusse@gmail.com>
2016-04-20 16:23:38 -0300

    Shouldn't work outside of ENV CHANNEL_NAME

--- app.rb
+++ app.rb
@@ -3,13 +3,16 @@ require 'httparty'
 require 'json'
 
 SLACK_WEBHOOK = ENV['SLACK_WEBHOOK_URL']
+CHANNEL_NAME = ENV['CHANNEL_NAME']
  
 get '/anon' do
+  return status(403) if params[:channel_name] != CHANNEL_NAME
   postback(params[:text], params[:channel_id])
   status 200
 end
  
 def postback(message, channel)
+
   HTTParty.post(SLACK_WEBHOOK, 
                 body: {
                   "text" => message.to_s,
